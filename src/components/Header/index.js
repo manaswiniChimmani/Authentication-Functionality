d958871b3b04d27ac54152a77216fe356e7ae063
Manaswini <chimmani.manaswini10@gmail.com>
2023-03-21 08:02:23 +0000

    authentication functionality

---
+++ src/components/Header/index.js
@@ -0,0 +1,19 @@
+// Write your JS code here
+import {Link} from 'react-router-dom'
+
+import './index.css'
+
+const Header = () => (
+  <nav>
+    <ul className="ul">
+      <Link to="/">
+        <li>Home</li>
+      </Link>
+      <Link to="/about">
+        <li>About</li>
+      </Link>
+    </ul>
+  </nav>
+)
+
+export default Header
---
+++ src/components/Login/index.js
@@ -0,0 +1,50 @@
+// Write your JS code here
+import {Component} from 'react'
+
+import Cookies from 'js-cookie'
+
+import {Redirect} from 'react-router-dom'
+import './index.css'
+
+class Login extends Component {
+  onSubmitSuccess = jwtToken => {
+    const {history} = this.props
+    Cookies.set('jwt_token', jwtToken, {expires: 30})
+    history.replace('/')
+  }
+
+  login = async () => {
+    const userDetails = {
+      username: 'rahul',
+      password: 'rahul@2021',
+    }
+
+    const url = 'https://apis.ccbp.in/login'
+    const options = {
+      method: 'POST',
+      body: JSON.stringify(userDetails),
+    }
+    const response = await fetch(url, options)
+    const data = await response.json()
+    if (response.ok === true) {
+      this.onSubmitSuccess(data.jwt_token)
+    }
+  }
+
+  render() {
+    const jwtToken = Cookies.get('jwt_token')
+    if (jwtToken !== undefined) {
+      return <Redirect to="/" />
+    }
+    return (
+      <div className="login-container">
+        <h1>Please Login</h1>
+        <button type="button" onClick={this.login}>
+          Login with Sample Creds
+        </button>
+      </div>
+    )
+  }
+}
+
+export default Login
